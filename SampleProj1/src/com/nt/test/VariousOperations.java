9c8f1d5119b58919d3d4fbb3c762205d61b529d7
Nagarjuna <Nagarjuna@192.168.0.5>
2017-05-01 20:16:41 +0530

    dev1---->modify the code 2

--- SampleProj1/src/com/nt/test/VariousOperations.java
+++ SampleProj1/src/com/nt/test/VariousOperations.java
@@ -7,6 +7,7 @@ public class VariousOperations {
 	public static void main(String[] args) {
 		
 		System.out.println("This main method1");
+		System.out.println("method2");
 		ArithmaticOperations aop=new ArithmaticOperations();
 		int result1=aop.add(20, 10);
 		System.out.println("addition is :"+result1);
